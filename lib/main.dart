302e8651a4863392eba929da69f46b905023a7a3
@Antriksh17 <shahantriksh22@gmail.com>
2026-02-26 11:30:00 +0530

    Api removed

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,7 @@ import 'package:traavaalay/View/SplashScreen.dart';
 
 Future<void> main() async {
    WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: FirebaseOptions(
-      apiKey: "AIzaSyC30I24x8plVGO46ZUA2J8dXj6W6g6K1ZM",
-      appId: "1:1042309955585:android:32fbd1008201cbc9a54000", 
-      messagingSenderId: "1042309955585", 
-      projectId: "beproject-f65ca")
-  );
+  
   
   runApp(const TravaalayApp());
 }
